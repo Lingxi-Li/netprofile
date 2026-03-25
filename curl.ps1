f96e69b8b132146c3c9f4df5173426d7c77d4587
Lingxi Li <lingxl@microsoft.com>
2026-03-25 22:28:50 +0800

    Drop pretransfer event and comment on RT count

--- curl.ps1
+++ curl.ps1
@@ -6,24 +6,22 @@ $keys =
     'namelookup',
     'connect',
     'appconnect',
-    'pretransfer',
     'starttransfer',
     'total'
 $options = ($keys | ForEach-Object { "%{time_$($_)}" }) -join ' '
 [double[]]$times = (curl -o NUL -w $options $URL) -split ' '
 
 ''
-'{0,-15} {1,10} {2,10}' -f '', 'time', 'delta'
+'{0,-15} {1,10} {2,10}' -f 'event', 'time', 'delta'
 '-------------------------------------'
 foreach ($i in 0..($keys.Length - 1)) {
     '{0,-15} {1,10:F3} {2,10:F3}' -f $keys[$i], $times[$i], `
         ($i -eq 0 ? $times[$i] : $times[$i] - $times[$i - 1])
 }
 
-# start
-#   ├─ DNS lookup        %{time_namelookup}
-#   ├─ TCP connect       %{time_connect}
-#   ├─ TLS handshake     %{time_appconnect}
-#   ├─ request prepared  %{time_pretransfer}
-#   ├─ first byte in     %{time_starttransfer}
-#   └─ done              %{time_total}
+# start                                        round-trip count
+#   ├─ DNS lookup      %{time_namelookup}      1
+#   ├─ TCP connect     %{time_connect}         1
+#   ├─ TLS connect     %{time_appconnect}      2/1 (TLS 1.2/1.3)
+#   ├─ first byte in   %{time_starttransfer}   1
+#   └─ done            %{time_total}
