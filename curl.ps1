47c0057acc8c3f4e8531eee00120959dc90b6b2a
Lingxi Li <lingxl@microsoft.com>
2026-03-26 03:09:24 +0800

    Record size and throughput

--- curl.ps1
+++ curl.ps1
@@ -10,15 +10,19 @@ $keys =
     'starttransfer',
     'total'
 $options = ($keys | ForEach-Object { "%{time_$($_)}" }) -join ' '
-[double[]]$times = (curl -o NUL -w $options --tls-max $TLSMax $URL || &{ exit }) -split ' '
+$out = (curl -o NUL -w "%{size_download} %{speed_download} $options" --tls-max $TLSMax $URL || &{ exit }) -split ' '
+[double[]]$times = $out[2..($out.Length - 1)]
 
-''
+'-------------------------------------'
 '{0,-15} {1,10} {2,10}' -f 'event', 'time', 'delta'
 '-------------------------------------'
 foreach ($i in 0..($keys.Length - 1)) {
     '{0,-15} {1,10:F3} {2,10:F3}' -f $keys[$i], $times[$i], `
         ($i -eq 0 ? $times[$i] : $times[$i] - $times[$i - 1])
 }
+'-------------------------------------'
+'{0,-18} {1,18:N0}' -f 'size (K)',         ($out[0] / 1KB)
+'{0,-18} {1,18:N0}' -f 'throughput (K/s)', ($out[1] / 1KB)
 
 # start                                        round-trip count
 #   ├─ DNS lookup      %{time_namelookup}      1
