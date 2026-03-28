eaa43dc0c46ae83d01c184538088a9ce7fa024ea
Lingxi Li <lingxl@microsoft.com>
2026-03-28 15:06:07 +0800

    Add metadata

--- getts.ps1
+++ getts.ps1
@@ -3,6 +3,10 @@ param (
 )
 
 $json = Get-Content $Path -Raw | ConvertFrom-Json
+$time = [DateTimeOffset]::Parse($json.start.timestamp.time).ToOffset([TimeSpan]::FromHours(8)).ToString('HHmm')
+'# timestamp    {0}' -f $time
+'# rcvbuf       {0:D0}K' -f ($json.start.rcvbuf_actual / 1KB)
+'# retransmits  {0:D}' -f $json.end.sum_sent.retransmits
 $json.intervals | Select-Object -ExpandProperty 'sum' | ForEach-Object {
     '{0:F3} {1:F1}' -f $_.end, ($_.bytes / $_.seconds / 1MB)
 }
