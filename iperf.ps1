90092117692ea51b0a8b6f4c87233744df7f30d2
Lingxi Li <lingxl@microsoft.com>
2026-03-28 17:08:20 +0800

    Add BufSize option

--- iperf.ps1
+++ iperf.ps1
@@ -1,8 +1,9 @@
 param (
     [Parameter(Mandatory)][string]$ServerIP,
-    [Parameter(Mandatory)][string]$FileName
+    [Parameter(Mandatory)][string]$FileName,
+    [Parameter(Mandatory)][string]$BufSize # like '1M'
 )
 
 $null = New-Item -ItemType Directory -Path 'iperf' -Force
-iperf3 -c $ServerIP -R -t 30 -i 0.2 --json > "iperf/$FileName.json"
+iperf3 -c $ServerIP -w $BufSize -R -t 30 -i 0.2 --json > "iperf/$FileName.json"
 .\getts.ps1 "iperf/$FileName.json" > "iperf/$FileName"
