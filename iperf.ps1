a8c941ea0434ddd8e639d75d21317168ce7ece2f
Lingxi Li <lingxl@microsoft.com>
2026-03-29 12:08:00 +0800

    Shorten interval to 100ms

--- iperf.ps1
+++ iperf.ps1
@@ -5,5 +5,5 @@ param (
 )
 
 $null = New-Item -ItemType Directory -Path 'iperf' -Force
-iperf3 -c $ServerIP -w $BufSize -R -t 30 -i 0.2 --json > "iperf/$FileName.json"
+iperf3 -c $ServerIP -w $BufSize -R -t 30 -i 0.1 --json > "iperf/$FileName.json"
 .\getts.ps1 "iperf/$FileName.json" > "iperf/$FileName"
