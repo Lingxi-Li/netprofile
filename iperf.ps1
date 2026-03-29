79cbf577d39a5baccabd1ca80e1a2033a766e510
Lingxi Li <lingxl@microsoft.com>
2026-03-29 19:18:41 +0800

    Add 'Multi' option

--- iperf.ps1
+++ iperf.ps1
@@ -1,9 +1,10 @@
 param (
     [Parameter(Mandatory)][string]$ServerIP,
     [Parameter(Mandatory)][string]$FileName,
-    [Parameter(Mandatory)][string]$BufSize # like '1M'
+    [Parameter(Mandatory)][string]$BufSize, # like '1M'
+    [int]$Multi = 1
 )
 
 $null = New-Item -ItemType Directory -Path 'iperf' -Force
-iperf3 -c $ServerIP -w $BufSize -R -t 30 -i 0.1 --json > "iperf/$FileName.json"
+iperf3 -c $ServerIP -w $BufSize -P $Multi -R -t 30 -i 0.1 --json > "iperf/$FileName.json"
 .\getts.ps1 "iperf/$FileName.json" > "iperf/$FileName"
